7afe0ac376df5fd39c92dd84d1ac54af87e42e65
Serg <e252xo@gmail.com>
2021-12-09 20:21:51 +0500

    delete #include<time.h>

--- src/main.c
+++ src/main.c
@@ -22,7 +22,6 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include <stdlib.h>
-#include <time.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
